25bfd203eae8b63eb4a94943a57356392be99b04
Akshaya Rajagopalan <a2rajago@edu.uwaterloo.ca>
2020-04-24 10:34:09 -0400

    cleaned up and added AI player with no functionality

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ from time import sleep
 
 class MyCardGame:
     def __init__(self):
-        self.canvas = Canvas(root, width = 1800, height = 1800)      
-        self.canvas.pack() 
+        self.canvas = Canvas(root, width = 1800, height = 1300)      
+        self.canvas.pack(pady=0) 
         self.deck_object = Deck() 
         self.deck = self.deck_object.shuffle()
         self.dummy_deck = self.deck_object.create_dummy_set()
@@ -14,13 +14,19 @@ class MyCardGame:
         self.people = []
 
         for i, image in enumerate(self.dummy_deck):
-            self.deck_position.append(self.canvas.create_image(i*25, 0, image=image, anchor=NW))
+            self.deck_position.append(self.canvas.create_image(i*5 , 418, image=image, anchor=CENTER))
             
         root.update()
         sleep(1)
-        self.people.append(Person(self.canvas))
-        self.people[-1].draw(root, self.canvas, self.deck_position, self.deck_object.deck, self.deck, 7, 0, 25, 700)
-            
+        self.people.append(Human(self.canvas, [500, 590, 465, 610], [500, 590, 655, 800]))
+        self.people.append(AI(self.canvas, [500, 590, 225, 370], [500, 590, 35, 180]))
+        root.update()
+        self.intial_draw()
+    
+    def intial_draw(self):
+        for i in range(0, 7*len(self.people), len(self.people)):
+            self.people[0].draw(root, self.canvas, self.deck_position, self.deck_object.deck, self.deck, i, int(i/len(self.people)), 45, 25, 770)
+            self.people[1].draw(root, self.canvas, self.deck_position, self.deck_object.deck, self.deck, i+1, int(i/len(self.people)), 45, 25, 70)
 
 root = Tk()
 my_gui = MyCardGame()
